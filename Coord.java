4968b4527393efe5eb485c89b15af45870989527
renoyin <renoyin9536@gmail.com>
2015-03-10 01:27:25 -0700

    basic methods for Coord and Snake

---
+++ Coord.java
@@ -0,0 +1,30 @@
+public class Coord {
+
+	private int x;
+	private int y;
+
+	public Coord(int X, int Y) {
+		x = X;
+		y = Y;
+	}
+
+	public Coord(Coord initial) {
+		x = initial.getX();
+		y = initial.getY();
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public boolean equals(Coord point) {
+		if (point.getX() == x && point.getY() == y)
+			return true;
+		else 
+			return false;
+	}
+}
--- Snake.java
+++ Snake.java
@@ -2,28 +2,33 @@ import java.util.ArrayList;
 import java.awt.*;
 
 public class Snake {
-	private ArrayList<Point> snake;
+	private ArrayList<Coord> snake;
 
 	public Snake(int initX, int initY) {
-		snake = new ArrayList<Point>();
-		snake.add(new Point(initX, initY));
+		snake = new ArrayList<Coord>();
+		snake.add(new Coord(initX, initY));
 	}
-/*
-	public void move(int x, int y) {
-		for (int i = 1; i < snake.length; i++)
-			snake.set(i, snake.get(i - 1)); 
-		snake.set(0, Point(x, y));
-	}
-*/
+
 	public boolean grow(int x, int y) {
-		for (int i = 0; i < snake.length; i++) 
+		for (int i = 0; i < snake.size(); i++) 
 			if (snake.get(i).getX == x && snake.get(i).getY == y)
 				return false;
-		snake.add(0, new Point(x, y));
+		snake.add(0, new Coord(x, y));
 		return true;
 	}
 
-	public Point getHead() {
+	public Coord getHead() {
 		return snake.get(0);
+	}
+
+	public ArrayList<Coord> getSnake() {
+		return snake;
+	}
+
+	public boolean intersect() {
+		for (int i = 1; i < snake.size(); i++)
+			if (snake.get(0).equals(snake.get(i)))
+				return true;
+		return false;
 	}
 }
