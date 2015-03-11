95fb0d5af5397d6370542d404ea4b3053378215a
jeromecao <lxc.jerome@gmail.com>
2015-03-10 22:23:48 -0700

    asdsdasd

--- SnakeMover.java
+++ SnakeMover.java
@@ -1,54 +1,84 @@
-public class Mover implements Runnable {
-    int w,h,p,wMax,hMax,speed;
-    SnakeGrid grid;
+import objectdraw.*;
+import java.awt.*;
+import java.awt.event.*;	
+
+public class SnakeMover implements KeyListener, Runnable {
+    int wMax,hMax,speed;
+    GameGrid grid;
     boolean going = true;
     Coord drct,cp;
+    Snake snake;
+    boolean turn = false;
    
     /**
      * Initializes the grid and the current target that is to be moved.
      * @param w The width of the grid
      * @param h The height of the grid
      * @param p The size of a cellar
-     * @param cx The horizontal location of the center red cell
-     * @param cy The vertical location of the center red cell
+     * @param cp
+     * @param drct
      * @param grid The reference of the current grid
      */
-    public Mover(int w,int h,int p,Coord cp,Coord drct,SnakeGrid grid){
-		this.w = w;
-		this.h = h;
-		this.p = p;
+    public SnakeMover(Coord cp,Coord drct,GameGrid grid, Snake snake) {
 		this.grid = grid;
 		this.cp = cp;
 		this.drct = drct;
-		wMax = w / p - 1;
-		hMax = h / p - 1;
+		this.snake = snake;
     }
     
     /**
-     * Executes the moving of the center red cell back and forth.
+     * Executes the moving of the snake.
      */
     public void run(){
-	
-		// drct is the current direction of moving.
-
+	    // Delay an amount of time
+		try { TimeUnit.MILLISECONDS.sleep(1000/speed);}
+		catch (InterruptedException e){};
 		if ((wMax == 0) || (hMax == 0)) going = false;
 		while (going) {
-		    // Delay an amount of time
-		    try { TimeUnit.MILLISECONDS.sleep(1000/speed);}
-		    catch (InterruptedException e){};
-		    // When it comes to either end, the direction reverses.
-		    if (cx == wMax) drct = -drct;
-		    if (cx == 0) drct = -drct;
-		    
+		    // determine snake's direction
+			if (turn == true) {
+				snake.grow(cp.getX()+drct.getX(),cp.getY()+drct.getY());
+				// add an obstacle.
+				// add 10 points
+			}
+			else {
+				snake.move();
+			}
 		    // Remove the cell (which has index 4)
 		    grid.clearCell(4);
 		    cx += drct;
 		    // Draw the new one
 		    grid.fillCell(cx,cy);
+
 		}
     }
 
     public void stop(){
 		going = false;
     }
+
+    // Implement the KeyListener Interface
+	public void keyPressed(KeyEvent e) {
+	}
+
+	public void keyReleased(KeyEvent e) {
+	}
+
+	public void keyTyped(KeyEvent e) {
+		Coord nd;
+		char key = e.getKeyChar();
+		if (!going) return; // don't move
+
+		if (key == 'l' || key == 'j')  
+		{
+			int change = 1;
+			if (key == 'l') change = -1;
+			if (drct.getX() == 0)
+				nd= new Coord(change*direction.getY(), 0);
+			else
+				nd = new Coord(0,-change*direction.getX());
+			drct = nd;
+			turn = true;
+		}
+	}
 }
