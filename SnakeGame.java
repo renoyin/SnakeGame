99880cbbf15ee8f1cdcded399cf14340ed3a4a2a
jeromecao <lxc.jerome@gmail.com>
2015-03-12 23:01:02 -0700

    button and slider

--- SnakeGame.java
+++ SnakeGame.java
@@ -7,7 +7,7 @@ import java.lang.Runnable;
 import java.util.concurrent.TimeUnit;
 import java.awt.event.*;
 
-public class SnakeGame extends JFrame {
+public class SnakeGame extends JFrame implements ActionListener, ChangeListener {
 	private static int w = 400, h = 400, p = 10;
     private static GameGrid grid;
    	private static SnakeMover move;
@@ -16,7 +16,10 @@ public class SnakeGame extends JFrame {
    	private static JLabel text1, text2, score, highScore;
    	private static JPanel topPanel;
    	private Container contentPane = getContentPane();
-
+   	private static JSlider slider;
+   	private static int sliderValue = 1;
+   	private static int speedMin = 0, speedMax = 20, speedInit = 1;
+   	private static JButton newGame, reset;
    	public SnakeGame(int width, int height, int pixel) {
 		super();
 		w = width;
@@ -35,7 +38,15 @@ public class SnakeGame extends JFrame {
 		topPanel.add(text2);
 		topPanel.add(highScore);
 		contentPane.add(topPanel, BorderLayout.NORTH);
-		contentPane.validate();		
+		contentPane.validate();
+
+		slider = new JSlider(speedMin, speedMax, speedInit);
+		slider.addChangeListener(this);
+		
+		newGame = new JButton("New Game");
+		newGame.addActionListener(this);
+		reset = new JButton("Reset!");
+		reset.addActionListener(this);
 
 		setSize(w + 10, h + 70);
 		snake = new Snake(w / (p * 2), h / (p * 2));
@@ -102,5 +113,21 @@ public class SnakeGame extends JFrame {
     
     public void setScore() {
     	score.setText(String.valueOf(grid.getPoints()));
+    }
+
+    @Override public void stateChanged(ChangeEvent e) {
+    	speedValue = slider.getValue();
+    	//update speed
+    }
+
+    @Override public void actionPerformed(ActionEvent e) {
+    	JButton sourceEvent = (JButton) event.getSource();
+
+    	if (sourceEvent == newGame) {
+    		//new game
+    	}
+    	else if (sourceEvent == reset) {
+    		//reset
+    	}
     }
 }
